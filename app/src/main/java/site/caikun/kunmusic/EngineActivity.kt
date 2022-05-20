fad4c38d81ee7f941ad12c3aa2a2f8c4fd3ff4c1
Caikun <tskunc@qq.com>
2022-05-20 20:46:20 +0800

    DataBinding

---
+++ app/src/main/java/site/caikun/kunmusic/EngineActivity.kt
@@ -0,0 +1,32 @@
+package site.caikun.kunmusic
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class EngineActivity<T : ViewDataBinding>(private val layout: Int) : AppCompatActivity() {
+
+    lateinit var binding: T
+    lateinit var root: View
+
+    override fun onStart() {
+        super.onStart()
+        supportActionBar?.hide()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        root = layoutInflater.inflate(layout, null)
+        binding = DataBindingUtil.bind(root)!!
+        setContentView(root)
+        init()
+    }
+
+    abstract fun init()
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
